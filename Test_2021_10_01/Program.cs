ae32c6f1723a518ed089d7cdb142622669a38e49
PetrovSerega <PetrovSerega@gmail.com>
2021-10-01 18:19:45 +0300

    Update Program.cs

--- Test_2021_10_01/Program.cs
+++ Test_2021_10_01/Program.cs
@@ -14,6 +14,7 @@ namespace Test_2021_10_01
             Console.WriteLine("Hello 2!");
             Console.WriteLine("Hello 4!");
             Console.WriteLine("Hello 3!");
+            Console.WriteLine("Hello 5!");
             Console.ReadKey();
         }
     }
